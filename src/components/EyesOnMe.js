661a22df7768897d57356c1890362f9315c215f4
Danny <danny.costelloe@gmail.com>
2021-04-07 16:29:19 -0400

    Done

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,8 @@
 // Code EyesOnMe Component Here
+function EyesOnMe(){
+    return(
+        <button onFocus ={() => console.log('Good!')} onBlur ={() =>console.log('Hey! Eyes on me!')}>EyesOnMe</button>
+    )
+}
+
+export default EyesOnMe
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,20 @@
 // Code Keypad Component Here
+function KeyPad(){
+        function handleChange(event) {
+          // console.log(`${event.target.name}: ${event.target.value}`);
+          return console.log("Entering password...", event.target.value)
+        }
+      
+        return (
+          <div>
+            <input
+              type="password"
+              name="search"
+              onChange={handleChange}
+              placeholder="Enter Password..."
+            />
+          </div>
+        );
+      }
+      
+export default KeyPad
